17def1f47eb1b65baf3874b7bb19c95269221501
Mateus Craveiro <mccraveiro@gmail.com>
2015-09-18 14:26:52 -0300

    Add signup view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'main#index'
+  get 'signup' => 'main#signup'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
