43d3a8e6fc015ceec06080e324b990b01b075aaf
Thiago Wittmann <thiwitt@gmail.com>
2015-09-22 23:05:13 -0300

    Login controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'main#index'
 
   resources :users
+  resources :sessions
   get 'cadastre-se' => 'users#new'
-  get 'login' => 'main#login'
+  get 'login' => 'sessions#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
