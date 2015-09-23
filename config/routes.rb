3e451aa548ebeabd90ab2f96b7607b25e40f2ece
Thiago Wittmann <thiwitt@gmail.com>
2015-09-22 23:19:03 -0300

    Signout controller created

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :sessions
   get 'cadastre-se' => 'users#new'
   get 'login' => 'sessions#new'
+  get 'sair' => 'sessions#destroy'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
