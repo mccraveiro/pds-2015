ddb45c9c420495342043024ed46b0dfaa8baa746
Thiago Wittmann <thiwitt@gmail.com>
2015-09-23 13:58:47 -0300

    Create email validation on signup
    close #18

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should validate email" do
+    user = User.new(name: 'test', email: 'test@test.com', password: 'test', password_confirmation: 'test')
+    assert user.valid?
+  end
+
+  test "should not validate email" do
+    user = User.new(name: 'test', email: 'test@gmail', password: 'test', password_confirmation: 'test')
+    assert_not user.valid?
+    assert_equal ["O email inserido não possui um formato válido."], user.errors.messages[:email]
+  end
 end
