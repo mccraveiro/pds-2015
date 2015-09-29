d78c8de72072f7e0c1a006c46296f147fb5d90f5
Thiago Wittmann <thiwitt@gmail.com>
2015-09-29 10:04:19 -0300

    Change api function Close #6

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -18,7 +18,10 @@ class MainController < ApplicationController
       [month]
     end
 
-    months.map { |m| call_api(1, city, year, m, domain, subdomain, nature) }
+    months.map { |m|
+      body = call_api(1, city, year, m, domain, subdomain, nature)
+      body[:get_total_despesa_response][:get_total_despesa_result]
+    }
   end
 
   def get_lista_despesa(city = "", year = "", month = "", domain = "", subdomain = "", nature = "")
@@ -32,7 +35,10 @@ class MainController < ApplicationController
       [month]
     end
 
-    months.map { |m| call_api(2, city, year, m, domain, subdomain, nature) }
+    months.map { |m|
+      body = call_api(2, city, year, m, domain, subdomain, nature)
+      body[:get_lista_despesa_response][:get_lista_despesa_result]
+    }
   end
 
   private
@@ -40,26 +46,21 @@ class MainController < ApplicationController
     # create a client for the service
     client = Savon.client(wsdl: 'http://transparenciaws.elasticbeanstalk.com/TransparenciaWS.asmx?WSDL')
 
-    if f == 1
-      response = client.call(:get_total_despesa, message: {
-        wNomeCidade: city,
-        wAno: year,
-        wMes: month,
-        wDominio: domain,
-        wSubDominio: subdomain,
-        wNatureza: nature
-      })
-      response.body[:get_total_despesa_response][:get_total_despesa_result]
+    fun = if f == 1
+      :get_total_despesa
     else
-      response = client.call(:get_lista_despesa, message: {
-        wNomeCidade: city,
-        wAno: year,
-        wMes: month,
-        wDominio: domain,
-        wSubDominio: subdomain,
-        wNatureza: nature
-      })
-      response.body[:get_lista_despesa_response][:get_lista_despesa_result]
+      :get_lista_despesa
     end
+
+    response = client.call(fun, message: {
+      wNomeCidade: city,
+      wAno: year,
+      wMes: month,
+      wDominio: domain,
+      wSubDominio: subdomain,
+      wNatureza: nature
+    })
+
+    response.body
   end
 end
